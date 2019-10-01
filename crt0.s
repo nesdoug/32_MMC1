9a3c7f9e495494d029206c473195e750a30b7c1b
nesdoug <dougfraker@gmail.com>
2019-10-01 10:14:18 -0400

    MMC1 example
    MMC1 example

---
+++ crt0.s
@@ -0,0 +1,351 @@
+; Startup code for cc65 and Shiru's NES library
+; based on code by Groepaz/Hitmen <groepaz@gmx.net>, Ullrich von Bassewitz <uz@cc65.org>
+
+
+
+
+; Edited to work with MMC1 code
+.define SOUND_BANK 6
+
+
+FT_BASE_ADR		= $0100		;page in RAM, should be $xx00
+FT_DPCM_OFF		= $f000		;$c000..$ffc0, 64-byte steps
+FT_SFX_STREAMS	= 1			;number of sound effects played at once, 1..4
+
+FT_THREAD       = 1		;undefine if you call sound effects in the same thread as sound update
+FT_PAL_SUPPORT	= 1		;undefine to exclude PAL support
+FT_NTSC_SUPPORT	= 1		;undefine to exclude NTSC support
+FT_DPCM_ENABLE  = 1		;undefine to exclude all DMC code
+FT_SFX_ENABLE   = 1		;undefine to exclude all sound effects code
+
+
+
+
+
+;REMOVED initlib
+;this called the CONDES function
+
+    .export _exit,__STARTUP__:absolute=1
+	.import push0,popa,popax,_main,zerobss,copydata
+
+; Linker generated symbols
+	.import __STACK_START__   ,__STACKSIZE__ ;changed
+	.import __ROM0_START__  ,__ROM0_SIZE__
+	.import __STARTUP_LOAD__,__STARTUP_RUN__,__STARTUP_SIZE__
+	.import	__CODE_LOAD__   ,__CODE_RUN__   ,__CODE_SIZE__
+	.import	__RODATA_LOAD__ ,__RODATA_RUN__ ,__RODATA_SIZE__
+	.import NES_MAPPER, NES_PRG_BANKS, NES_CHR_BANKS, NES_MIRRORING
+
+	.importzp _PAD_STATE, _PAD_STATET ;added
+    .include "zeropage.inc"
+
+;	.import _set_prg_bank, _get_prg_bank, _set_chr_bank_0, _set_chr_bank_1
+;	.import _set_mirroring, _set_mmc1_ctrl
+
+
+
+
+PPU_CTRL	=$2000
+PPU_MASK	=$2001
+PPU_STATUS	=$2002
+PPU_OAM_ADDR=$2003
+PPU_OAM_DATA=$2004
+PPU_SCROLL	=$2005
+PPU_ADDR	=$2006
+PPU_DATA	=$2007
+PPU_OAM_DMA	=$4014
+PPU_FRAMECNT=$4017
+DMC_FREQ	=$4010
+CTRL_PORT1	=$4016
+CTRL_PORT2	=$4017
+
+OAM_BUF		=$0200
+PAL_BUF		=$01c0
+VRAM_BUF	=$0700
+
+
+
+.segment "ZEROPAGE"
+
+NTSC_MODE: 			.res 1
+FRAME_CNT1: 		.res 1
+FRAME_CNT2: 		.res 1
+VRAM_UPDATE: 		.res 1
+NAME_UPD_ADR: 		.res 2
+NAME_UPD_ENABLE: 	.res 1
+PAL_UPDATE: 		.res 1
+PAL_BG_PTR: 		.res 2
+PAL_SPR_PTR: 		.res 2
+SCROLL_X: 			.res 1
+SCROLL_Y: 			.res 1
+SCROLL_X1: 			.res 1
+SCROLL_Y1: 			.res 1
+PAD_STATE: 			.res 2		;one byte per controller
+PAD_STATEP: 		.res 2
+PAD_STATET: 		.res 2
+PPU_CTRL_VAR: 		.res 1
+PPU_CTRL_VAR1: 		.res 1
+PPU_MASK_VAR: 		.res 1
+RAND_SEED: 			.res 2
+FT_TEMP: 			.res 3
+
+TEMP: 				.res 11
+SPRID:				.res 1
+
+PAD_BUF		=TEMP+1
+
+PTR			=TEMP	;word
+LEN			=TEMP+2	;word
+NEXTSPR		=TEMP+4
+SCRX		=TEMP+5
+SCRY		=TEMP+6
+SRC			=TEMP+7	;word
+DST			=TEMP+9	;word
+
+RLE_LOW		=TEMP
+RLE_HIGH	=TEMP+1
+RLE_TAG		=TEMP+2
+RLE_BYTE	=TEMP+3
+
+;nesdoug code requires
+VRAM_INDEX:			.res 1
+META_PTR:			.res 2
+DATA_PTR:			.res 2
+
+
+
+
+
+.segment "HEADER"
+
+    .byte $4e,$45,$53,$1a
+	.byte <NES_PRG_BANKS
+	.byte <NES_CHR_BANKS
+	.byte <NES_MIRRORING|(<NES_MAPPER<<4)
+	.byte <NES_MAPPER&$f0
+	.byte 1 ;8k of PRG RAM
+	.res 7,0
+
+
+; linker complains if I don't have at least one mention of each bank
+.segment "ONCE"
+.segment "BANK0"
+.segment "BANK1"
+.segment "BANK2"
+.segment "BANK3"
+.segment "BANK4"
+.segment "BANK5"
+.segment "BANK6"
+
+.segment "STARTUP"
+
+start:
+_exit:
+
+    sei
+	cld
+	ldx #$40
+	stx CTRL_PORT2
+    ldx #$ff
+    txs
+    inx
+    stx PPU_MASK
+    stx DMC_FREQ
+    stx PPU_CTRL		;no NMI
+	
+	
+; MMC1 reset
+
+	lda #$80 ; reset all latches
+	sta $8000
+	sta $a000
+	sta $c000
+	sta $e000
+	
+	lda #$1f 	; set control to horizontal mirroring, 
+				; last bank $c000, $8000 swappable
+				; CHR in 4k and 4k mode
+	jsr _set_mmc1_ctrl
+	
+	lda #$00 ;CHR bank #0 for first tileset
+	jsr _set_chr_bank_0
+	
+	lda #$01 ;CHR bank #1 for second tileset
+	jsr _set_chr_bank_1
+	
+	lda #$00 ;PRG bank #0 at $8000
+	jsr _set_prg_bank
+	
+	
+	;x is still zero
+
+initPPU:
+    bit PPU_STATUS
+@1:
+    bit PPU_STATUS
+    bpl @1
+@2:
+    bit PPU_STATUS
+    bpl @2
+
+clearPalette:
+	lda #$3f
+	sta PPU_ADDR
+	stx PPU_ADDR
+	lda #$0f
+	ldx #$20
+@1:
+	sta PPU_DATA
+	dex
+	bne @1
+
+clearVRAM:
+	txa
+	ldy #$20
+	sty PPU_ADDR
+	sta PPU_ADDR
+	ldy #$10
+@1:
+	sta PPU_DATA
+	inx
+	bne @1
+	dey
+	bne @1
+
+clearRAM:
+    txa
+@1:
+    sta $000,x
+    sta $100,x
+    sta $200,x
+    sta $300,x
+    sta $400,x
+    sta $500,x
+    sta $600,x
+    sta $700,x
+    inx
+    bne @1
+
+	lda #4
+	jsr _pal_bright
+	jsr _pal_clear
+	jsr _oam_clear
+
+    jsr	zerobss
+	jsr	copydata
+
+    lda #<(__STACK_START__+__STACKSIZE__) ;changed
+    sta	sp
+    lda	#>(__STACK_START__+__STACKSIZE__)
+    sta	sp+1            ; Set argument stack ptr
+
+;	jsr	initlib
+; removed. this called the CONDES function
+
+	lda #%10000000
+	sta <PPU_CTRL_VAR
+	sta PPU_CTRL		;enable NMI
+	lda #%00000110
+	sta <PPU_MASK_VAR
+
+waitSync3:
+	lda <FRAME_CNT1
+@1:
+	cmp <FRAME_CNT1
+	beq @1
+
+detectNTSC:
+	ldx #52				;blargg's code
+	ldy #24
+@1:
+	dex
+	bne @1
+	dey
+	bne @1
+
+	lda PPU_STATUS
+	and #$80
+	sta <NTSC_MODE
+
+	jsr _ppu_off
+
+	lda #0
+	ldx #0
+	jsr _set_vram_update
+
+	lda #$fd
+	sta <RAND_SEED
+	sta <RAND_SEED+1
+
+	lda #0
+	sta PPU_SCROLL
+	sta PPU_SCROLL
+	
+	
+	
+	lda #SOUND_BANK ;PRG bank where all the music stuff is there
+					;SOUND_BANK is defined above
+	jsr _set_prg_bank
+	
+	ldx #<music_data
+	ldy #>music_data
+	lda <NTSC_MODE
+	jsr FamiToneInit
+
+	ldx #<sounds_data
+	ldy #>sounds_data
+	jsr FamiToneSfxInit
+	
+	lda #$00 ;PRG bank #0 at $8000, back to basic
+	jsr _set_prg_bank
+	
+	;for split screens with different CHR bank at top... disable it
+	jsr _unset_nmi_chr_tile_bank
+
+	jmp _main			;no parameters
+	
+	
+
+	.include "MMC1/mmc1_macros.asm"
+	.include "MMC1/bank_helpers.asm"
+	.include "LIB/neslib.s"
+	.include "LIB/nesdoug.s"
+	
+
+
+
+	
+; I put all the music stuff on bank 6
+; all the music functions swap in bank 6
+	
+.segment "BANK6"	
+	.include "MUSIC/famitone2.s"
+
+music_data:
+	.include "MUSIC/TestMusic3.s"
+
+sounds_data:
+	.include "MUSIC/SoundFx.s"
+
+
+	
+	
+.segment "SAMPLES"
+	.incbin "MUSIC/BassDrum.dmc"
+
+
+
+.segment "VECTORS"
+
+    .word nmi	;$fffa vblank nmi
+    .word start	;$fffc reset
+   	.word irq	;$fffe irq / brk
+
+
+.segment "CHARS"
+
+	.incbin "Alpha.chr"
+	.incbin "Alpha2.chr"
+; the CHARS segment is much bigger, and I could have 
+; incbin-ed many more chr files
+	
---
+++ hello_mmc1.c
@@ -0,0 +1,268 @@
+/*	example of MMC1 for cc65
+ *	Doug Fraker 2019
+ */	
+ 
+#include "LIB/neslib.h"
+#include "LIB/nesdoug.h" 
+#include "MMC1/bank_helpers.h"
+#include "MMC1/bank_helpers.c"
+
+enum{BANK_0, BANK_1, BANK_2, BANK_3, BANK_4, BANK_5, BANK_6};
+// 7 shouldn't be needed, that's the fixed bank, just call it normally
+
+
+ 
+#pragma bss-name(push, "ZEROPAGE")
+
+// GLOBAL VARIABLES
+
+unsigned char arg1;
+unsigned char arg2;
+unsigned char pad1;
+unsigned char pad1_new;
+unsigned char char_state;
+unsigned char song;
+unsigned char sound;
+unsigned char pauze;
+
+#pragma bss-name(pop)
+
+
+#pragma bss-name(push, "XRAM")
+// extra RAM at $6000-$7fff
+unsigned char wram_array[0x2000];
+
+#pragma bss-name(pop)
+
+
+
+
+#define BLACK 0x0f
+#define DK_GY 0x00
+#define LT_GY 0x10
+#define WHITE 0x30
+
+const unsigned char palette[]={
+BLACK, DK_GY, LT_GY, WHITE,
+0,0,0,0,
+0,0,0,0,
+0,0,0,0
+}; 
+
+
+// test putting things in other banks
+#pragma rodata-name ("BANK0")
+#pragma code-name ("BANK0")
+const unsigned char TEXT0[]="BANK0";
+
+void function_bank0(void){
+	ppu_off();
+	vram_adr(NTADR_A(1,4));
+	vram_write(TEXT0,sizeof(TEXT0));
+	ppu_on_all();
+}
+
+
+#pragma rodata-name ("BANK1")
+#pragma code-name ("BANK1")
+const unsigned char TEXT1[]="BANK1";
+
+void function_bank2(void);	// a prototype can be anywhere
+							// as long as it's above the code that uses it
+
+void function_bank1(void){
+	ppu_off();
+	vram_adr(NTADR_A(1,6));
+	vram_write(TEXT1,sizeof(TEXT1));
+	ppu_on_all();
+	
+	banked_call(BANK_2, function_bank2);
+}
+
+
+#pragma rodata-name ("BANK2")
+#pragma code-name ("BANK2")
+const unsigned char TEXT2[]="BANK2";
+
+void function_bank2(void){
+	ppu_off();
+	vram_adr(NTADR_A(1,8));
+	vram_write(TEXT2,sizeof(TEXT2));
+	ppu_on_all();
+}
+
+
+#pragma rodata-name ("BANK3")
+#pragma code-name ("BANK3")
+const unsigned char TEXT3[]="BANK3";
+
+void function_bank3(void){
+	 
+	ppu_off();
+	vram_adr(NTADR_A(1,10));
+	vram_write(TEXT3,sizeof(TEXT3));
+	
+	vram_put(0);
+	vram_put(arg1); // these args were passed via globals
+	vram_put(arg2);
+	ppu_on_all();
+}
+
+
+#pragma rodata-name ("BANK4")
+#pragma code-name ("BANK4")
+const unsigned char TEXT4[]="BANK4";
+
+void function_bank4(void){
+	 
+	ppu_off();
+	vram_adr(NTADR_A(1,12));
+	vram_write(TEXT4,sizeof(TEXT4));
+	ppu_on_all();
+}
+
+#pragma rodata-name ("BANK5")
+#pragma code-name ("BANK5")
+const unsigned char TEXT5[]="BANK5";
+const unsigned char TEXT5B[]="ALSO THIS";
+
+void function_2_bank5(void){
+	vram_adr(NTADR_A(8,14));
+	vram_write(TEXT5B,sizeof(TEXT5B));
+}
+
+void function_bank5(void){
+	 
+	ppu_off();
+	vram_adr(NTADR_A(1,14));
+	vram_write(TEXT5,sizeof(TEXT5));
+	
+	// use a regular function call to call a function in
+	// the same bank
+	function_2_bank5();
+	
+	ppu_on_all();
+}
+
+#pragma rodata-name ("BANK6")
+#pragma code-name ("BANK6")
+const unsigned char TEXT6[]="BANK6";
+
+void function_bank6(void){
+	 
+	ppu_off();
+	vram_adr(NTADR_A(1,16));
+	vram_write(TEXT6,sizeof(TEXT6));
+	
+	vram_put(0);
+	vram_put(wram_array[0]); // testing the $6000-7fff area
+	vram_put(wram_array[2]); // should print A, C
+	ppu_on_all();
+}
+	
+	
+	
+	
+// the fixed bank, bank 7
+	
+#pragma rodata-name ("CODE")
+#pragma code-name ("CODE")	
+
+const unsigned char text[]="BACK IN FIXED BANK, 7";
+
+void draw_sprites (void);
+
+
+
+
+void main (void) {
+	
+	song = 0;
+	music_play(song);
+
+	wram_array[0] = 'A'; // put some values at $6000-7fff
+	wram_array[2] = 'C'; // for later testing
+	
+	draw_sprites();
+	
+	ppu_off(); // screen off
+	pal_bg(palette); //	load the BG palette
+	pal_spr(palette); // load the sprite palette
+	ppu_on_all(); // turn on screen
+	
+	
+	// calling functions in other banks
+	
+	banked_call(BANK_0, function_bank0);
+	banked_call(BANK_1, function_bank1);
+	// banked_call(BANK_2, function_bank2); // moved to bank 1
+	
+	arg1 = 'G'; // must pass arguments with globals
+	arg2 = '4';
+	banked_call(BANK_3, function_bank3);
+	banked_call(BANK_4, function_bank4);
+	banked_call(BANK_5, function_bank5);
+	banked_call(BANK_6, function_bank6);
+
+	 
+	ppu_off(); // screen off
+	vram_adr(NTADR_A(1,18));
+	vram_write(text,sizeof(text)); // code running from the fixed bank
+	
+	
+	// set the bank 0 at $8000-bfff
+	// with a swappable bank in place, we can read it from the fixed bank
+	set_prg_bank(0); 
+	vram_adr(NTADR_A(1,20));
+	vram_write(TEXT0,sizeof(TEXT0)); // this array is in bank 0
+	ppu_on_all(); //	turn on screen
+	
+	while (1){ // infinite loop
+		ppu_wait_nmi();
+		
+		pad1 = pad_poll(0);
+		pad1_new = get_pad_new(0);
+		
+		if(pad1_new & PAD_START){
+			++char_state;
+			char_state = char_state & 3; // keep 0-3
+			
+			set_chr_bank_0(char_state); // switch the BG bank
+			// note just the tileset #0 is changed,
+			// the sprite bank would have to be changed with
+			// set_chr_bank_1();
+			
+			sample_play(1);
+		}
+		
+		if(pad1_new & PAD_A){
+			++song;
+			song = song & 1; // keep 0 or 1
+			music_play(song);
+		}
+		
+		if(pad1_new & PAD_B){
+			++sound;
+			sound = sound & 1; // keep 0 or 1
+			sfx_play(sound, 0);
+		}
+		
+		if(pad1_new & PAD_SELECT){
+			++pauze;
+			pauze = pauze & 1; // keep 0 or 1
+			music_pause(pauze);
+		}
+	}
+}
+
+	
+// testing sprites using the second tileset	
+void draw_sprites (void) { 
+	bank_spr(1);
+	oam_clear();
+	oam_spr(0x50,0x10,0,0);
+	oam_spr(0x58,0x10,1,0);
+	oam_spr(0x50,0x18,0x10,0);
+	oam_spr(0x58,0x18,0x11,0);
+}
+
